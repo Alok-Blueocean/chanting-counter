838066ed0a700ce16f6c624edfdfe6a07e2131a5
Alok Ranjan <alokrnjn3@gmail.com>
2023-08-28 16:40:29 +0530

    changes button color
    changes button color

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ let round_count = document.getElementById('rounds')
 let count = 0
 let rounds = 0
 function increment(){
-console.log(bead_count.innerText);
-
-console.log(round_count.innerText);
-
-count = count + 1;
-bead_count.innerText = count;
-rounds = rounds+ parseInt(count/108);
-if (count==108){
-    count = 0;
-}
-round_count.innerText = rounds;
+            count = count + 1;
+            bead_count.innerText = count;
+            rounds = rounds+ parseInt(count/108);
+            if (count==108){
+                count = 0;
+            }
+            round_count.innerText = rounds;
 };
 
 // const body_click = document.getElementById('body_click');  
